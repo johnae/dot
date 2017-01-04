b8f774f337ac652c176334ed64e416a11cbc59ff
John Axel Eriksson <john@insane.se>
2017-01-04 13:07:19 +0100

    Better zsh tab completion + menu

--- .profile.d/zsh/comp.zsh
+++ .profile.d/zsh/comp.zsh
@@ -1,5 +1,12 @@
 fpath=(/usr/share/fzf $fpath)
 zstyle :compinstall filename ~/.zshrc
+# case insensitive matching
+zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}'
+# enable menu (eg tabbing through options)
+zstyle ':completion:*' menu select
+# when a character is ambiguous just select first (just tab through)
+setopt menu_complete
+
 
 #autoload -Uz git
 zmodload zsh/complist
