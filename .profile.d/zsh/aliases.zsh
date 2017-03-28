c3fba658136dc1cd4d206a81e1782893cbfc0fc9
John Axel Eriksson <john@insane.se>
2017-03-28 13:29:24 +0200

    Add ls aliases l and ll.

--- .profile.d/zsh/aliases.zsh
+++ .profile.d/zsh/aliases.zsh
@@ -5,6 +5,8 @@ if command -V dircolors >/dev/null 2>&1; then
 	alias ls="ls -F --color=auto --group-directories-first"
 fi
 
+alias l="ls -lh"
+alias ll="ls -lah"
 alias grep="grep --color=auto"
 alias fgrep="fgrep --color=auto"
 alias egrep="egrep --color=auto"
