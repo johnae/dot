c04580de55d38bcd010b988537f7c77ce19f615c
John Axel Eriksson <john@insane.se>
2017-07-16 09:35:21 +0200

    Fix shell colors on FreeBSD/MacOS.

--- .profile.d/zsh/aliases.zsh
+++ .profile.d/zsh/aliases.zsh
@@ -1,17 +1,27 @@
 # Color aliases
-if command -V dircolors >/dev/null 2>&1; then
-	eval "$(dircolors -b)"
-	# Only alias ls colors if dircolors is installed
-	alias ls="ls -F --color=auto --group-directories-first"
+
+if [ "$(uname)" = "Linux" ]; then
+  if command -V dircolors >/dev/null 2>&1; then
+    eval "$(dircolors -b)"
+    # Only alias ls colors if dircolors is installed
+    alias ls="ls -F --color=auto --group-directories-first"
+  fi
 fi
 
 alias l="ls -lh"
 alias ll="ls -lah"
-alias grep="grep --color=auto"
-alias fgrep="fgrep --color=auto"
-alias egrep="egrep --color=auto"
-# make less accept color codes and re-output them
-alias less="less -R"
+if [ "$(uname)" = "Linux" ]; then
+  alias grep="grep --color=auto"
+  alias fgrep="fgrep --color=auto"
+  alias egrep="egrep --color=auto"
+  # make less accept color codes and re-output them
+  alias less="less -R"
+else
+  CLICOLOR="YES"
+  export CLICOLOR
+  LSCOLORS="ExGxFxdxCxDxDxhbadExEx"
+  export LSCOLORS
+fi
 
 alias vim=nvim
 alias spotify="spotify --force-device-scale-factor=1.7"
