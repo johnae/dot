77c47b0c1507d82801095a3562762201da4ecbb4
John Axel Eriksson <john@insane.se>
2017-09-07 13:54:54 +0200

    Ensure a mark is named - fail otherwise.

--- .profile.d/zsh/marks.zsh
+++ .profile.d/zsh/marks.zsh
@@ -1,39 +1,43 @@
 if [[ -z $BOOKMARKS_FILE ]] ; then
-    export BOOKMARKS_FILE="$HOME/.zsh_bookmarks"
+  export BOOKMARKS_FILE="$HOME/.zsh_bookmarks"
 fi
 
 if [[ ! -f $BOOKMARKS_FILE ]]; then
-    touch $BOOKMARKS_FILE
+  touch $BOOKMARKS_FILE
 fi
 
 function mark() {
-    echo $@ : $(pwd) >> $BOOKMARKS_FILE
+  if [ -z "$@" ]; then
+    echo "Please name the mark"
+    return 1
+  fi
+  echo $@ : $(pwd) >> $BOOKMARKS_FILE
 }
 
 function jump_to_mark() {
-    local jumpline
-    jumpline=$(cat ${BOOKMARKS_FILE} | $(fzfcmd) --bind=ctrl-y:accept --tac)
-    if [[ -n ${jumpline} ]]; then
-        local jumpdir
-        jumpdir=$(echo "${jumpline}" | sed -n "s/.* : \(.*\)$/\1/p" | sed "s#~#$HOME#")
-        perl -p -i -e "s#${jumpline}\n##g" $BOOKMARKS_FILE
-        cd "${jumpdir}" && echo ${jumpline} >> $BOOKMARKS_FILE
-    fi
-    zle && zle reset-prompt
+  local jumpline
+  jumpline=$(cat ${BOOKMARKS_FILE} | $(fzfcmd) --bind=ctrl-y:accept --tac)
+  if [[ -n ${jumpline} ]]; then
+    local jumpdir
+    jumpdir=$(echo "${jumpline}" | sed -n "s/.* : \(.*\)$/\1/p" | sed "s#~#$HOME#")
+    perl -p -i -e "s#${jumpline}\n##g" $BOOKMARKS_FILE
+    cd "${jumpdir}" && echo ${jumpline} >> $BOOKMARKS_FILE
+  fi
+  zle && zle reset-prompt
 }
 
 function delete_mark()  {
-    local marks_to_delete line
-    marks_to_delete=$(cat $BOOKMARKS_FILE | $(fzfcmd) -m --bind=ctrl-y:accept,ctrl-t:toggle-up --tac)
+  local marks_to_delete line
+  marks_to_delete=$(cat $BOOKMARKS_FILE | $(fzfcmd) -m --bind=ctrl-y:accept,ctrl-t:toggle-up --tac)
 
-    if [[ -n ${marks_to_delete} ]]; then
-        while read -r line; do
-            perl -p -i -e "s#${line}\n##g" $BOOKMARKS_FILE
-        done <<< "$marks_to_delete"
+  if [[ -n ${marks_to_delete} ]]; then
+    while read -r line; do
+      perl -p -i -e "s#${line}\n##g" $BOOKMARKS_FILE
+    done <<< "$marks_to_delete"
 
-        echo "** The following marks were deleted **"
-        echo "${marks_to_delete}"
-    fi
+    echo "** The following marks were deleted **"
+    echo "${marks_to_delete}"
+  fi
 }
 
 zle -N jump_to_mark
