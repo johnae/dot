4cdd81332c85668ef0c264109aae491257bdbe24
John Axel Eriksson <john@insane.se>
2017-10-22 17:50:16 +0200

    zsh: remove marks feature, add project jump feature.
    The new feature works by scanning for .git/config files and assuming
    all such .git dirs are contained within project directories.

---
+++ .profile.d/zsh/project_jump.zsh
@@ -0,0 +1,11 @@
+function jump_to_project(){
+    local jumpline
+    jumpline=$(find $PROJECTS -type f | grep ".git/config" | sed 's|/\.git/config||g' | $(fzfcmd) --bind=ctrl-y:accept --tac)
+    if [[ -n ${jumpline} ]]; then
+      cd $jumpline
+    fi
+    zle && zle reset-prompt
+}
+
+zle -N jump_to_project
+bindkey '^g' jump_to_project
